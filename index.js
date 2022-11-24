aea3f3956889eb862d02fec04fc47cb41fca11bb
TealeafB <115798288+TealeafB@users.noreply.github.com>
2022-11-24 15:43:29 +0100

    Added snare, guitar and volume values. Fixed some bugs.

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ cmp.connect(c.destination);
 var bs = c.createBufferSource();
 bs.loop = true;
 
-var BPM = 60;
+var BPM = 120;
 /*var style;
 var key;
 var scale;
@@ -16,6 +16,7 @@ var index=0;
 var notes;
 var sub;
 var refreshIntervalId = 0;
+var intensityIndex = 0;
 
 function createTimeSignatureDen(time) {
     timeSignatureDen = time;
@@ -23,20 +24,43 @@ function createTimeSignatureDen(time) {
 
 function kick() {
     var audio = new Audio('bassy_kick.wav');
+    volume(audio);
     audio.play();
 }
 
 function cymbal(){
     var audio = new Audio('new_cymbal.wav');
+    volume(audio);
     audio.play();
+    intensityIndex++;
 }
 
-/*function snare(){
+function snare(){
     var audio = new Audio('cage_snare.wav');
     audio.play();
 }
 
-function hat(){
+function guitar(){
+    var audio = new Audio('HMRhyChugA-E Lo.wav');
+    volume(audio);
+    audio.play();
+}
+
+function volume(audio){
+    if (intensityIndex==0){
+        audio.volume=1;
+    }
+    else if (intensityIndex==Math.ceil(timeSignatureDen/(timeSignatureNum*4))){
+        audio.volume=0.5;
+    }
+    else if (intensityIndex==Math.ceil(timeSignatureDen/(timeSignatureNum*2))){
+        audio.volume=0.8;
+    }
+    else if (intensityIndex==Math.ceil(timeSignatureDen/timeSignatureNum)){
+        audio.volume=0.3;
+    }
+}
+/*function hat(){
     var audio = new Audio('man_hat.wav');
     audio.play();
 }*/
@@ -83,24 +107,26 @@ function play() {
     if (refreshIntervalId){
         clearInterval(refreshIntervalId);
         refreshIntervalId = 0;
+        index=0;
+        intensityIndex=0;
     }
     else {
         sub = Math.ceil(Math.random() * 7);
         if (sub % 7 == 0) {
-            sub = 28;
+            sub = 14;
         } else if (sub % 5 == 0) {
-            sub = 40;
+            sub = 10;
         } else if (sub % 3 == 0) {
-            sub = 24;
+            sub = 12;
         } else if (sub % 2 == 0) {
             sub = 16;
         } else if (sub == 1) {
-            sub = 16;
+            sub = 8;
         }
 
         notes = timeSignatureNum * sub / timeSignatureDen;
         var pattern = Math.floor(Math.random() * Math.pow(2, notes));
-        refreshIntervalId = setInterval(render, 60000 / (BPM * sub / 4), pattern)
+        refreshIntervalId = setInterval(render, 240000 / (BPM * sub), pattern)
     }
 
     // VECCHIA VERSIONE
@@ -138,16 +164,21 @@ function toggleOn (e) {
 function render(pattern){
     let patternBinary = pattern.toString(2);
 
-    if (patternBinary.charAt(index) - '0'){
+    if (patternBinary.charAt(index) - '0' && intensityIndex!=3){
         kick();
+        guitar();
     }
     if(index%(sub/4)==0){
         cymbal();
+        if (intensityIndex==3){
+            snare();
+        }
     }
 
     index++;
     if (index>=notes){
         index=0;
+        intensityIndex=0;
     }
 }
 
