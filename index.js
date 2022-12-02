8937ae9274d9d0dfb3a8ae93af42bcc3db775431
riccardopomarico <riccardo.pomarico@mail.polimi.it>
2022-12-02 12:53:22 +0100

    Updated music sheet feature

--- index.js
+++ index.js
@@ -21,10 +21,12 @@ var complexity;
 
 function createTimeSignatureNum() {
     timeSignatureNum = parseInt(document.getElementById('timesignum').value);
+    //stave.addClef("treble").addTimeSignature(timeSignatureNum + "/" + timeSignatureDen);
 }
 
 function createTimeSignatureDen() {
     timeSignatureDen = parseInt(document.getElementById('timesigden').value);
+    //stave.addClef("treble").addTimeSignature(timeSignatureNum + "/" + timeSignatureDen);
 }
 
 function changeBPM() {
@@ -123,12 +125,17 @@ function volume(audio){
 function play() {
 
     if (refreshIntervalId){
+        document.getElementById("some-div-id").innerHTML = "";
+
         clearInterval(refreshIntervalId);
         refreshIntervalId = 0;
         index=0;
         intensityIndex=0;
     }
     else {
+
+        createStave();
+
         sub = Math.ceil(Math.random() * 7);
         if (sub % 7 == 0) {
             sub = 14;
@@ -200,6 +207,49 @@ function render(pattern){
     }
 }
 
+VF = Vex.Flow;
+
+
+function createStave() {
+// We created an object to store the information about the workspace
+    var WorkspaceInformation = {
+        // The div in which you're going to work
+        div: document.getElementById("some-div-id"),
+        // Vex creates a svg with specific dimensions
+        canvasWidth: 500,
+        canvasHeight: 500
+    };
+
+// Create a renderer with SVG
+    var renderer = new VF.Renderer(
+        WorkspaceInformation.div,
+        VF.Renderer.Backends.SVG
+    );
+
+// Use the renderer to give the dimensions to the SVG
+    renderer.resize(WorkspaceInformation.canvasWidth, WorkspaceInformation.canvasHeight);
+
+// Expose the context of the renderer
+    var context = renderer.getContext();
+
+// And give some style to our SVG
+    context.setFont("Arial", 10, "").setBackgroundFillStyle("#eed");
+
+
+    /**
+     * Creating a new stave
+     */
+// Create a stave of width 400 at position x10, y40 on the SVG.
+    var stave = new VF.Stave(10, 40, 400);
+
+    stave.addClef("treble").addTimeSignature(timeSignatureNum + "/" + timeSignatureDen);
+    stave.setContext(context).draw();
+// Add a clef and time signature.
+// stave.addClef("treble").addTimeSignature("4/4");
+//stave.addClef("treble").addTimeSignature(timeSignatureNum + "/" + timeSignatureDen);
+// Set the context of the stave our previous exposed context and execute the method draw !
+// stave.setContext(context).draw();
+}
 /* function render(pattern1, pattern2, pattern3, pattern4){
     var pattern
     if (index<4) {pattern=pattern1}
